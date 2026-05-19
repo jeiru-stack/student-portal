4e83c24eb8af20b110eba9a8e78531af2d3e2812
unknown <jrbackup07@gmail.com>
2026-05-19 09:48:30 +0800

    layouts and pages

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,105 @@
     <v-app>
         <v-main>
             <v-container>
-                
+                <v-row>
+                    <v-col>
+                        <v-text-field
+                            label="Student Number"
+                            variant="outlined"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col
+                        ols="12"
+                        md="4"
+                    >
+                        <v-text-field
+                            label="Last Name"
+                            variant="outlined"
+                        ></v-text-field>                
+                    </v-col>
+                    <v-col
+                        cols="12"
+                        md="4"
+                    >
+                        <v-text-field
+                            label="First Name"
+                            variant="outlined"
+                        ></v-text-field>
+                    </v-col>
+                                        <v-col
+                        cols="12"
+                        md="4"
+                    >
+                        <v-text-field
+                            label="Middle Name"
+                            variant="outlined"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col
+                        cols="12"
+                        md="4"
+                    >
+                        <v-combobox
+                            label="Course"
+                            :items="['BSCS', 'DCT']"
+                            variant="outlined"
+                        ></v-combobox>
+                    </v-col>
+                    <v-col
+                        cols="12"
+                        md="4"
+                    >
+                        <v-combobox
+                            label="Year"
+                            :items="['1st Year', '2nd Year', '3rd Year', '4th Year']"
+                            variant="outlined"
+                        ></v-combobox>
+                    </v-col>
+                    <v-col
+                        cols="12"
+                        md="4"
+                    >
+                        <v-combobox
+                            label="Section"
+                            :items="['A', 'B', 'C', 'D']"
+                            variant="outlined"
+                        ></v-combobox>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col
+                        cols="12"
+                        md="4"
+                    >
+                        <v-text-field
+                            label="Address"
+                            variant="outlined"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col
+                        cols="12"
+                        md="4"
+                    >
+                        <v-text-field
+                            label="Contact No."
+                            variant="outlined"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col
+                        cols="12"
+                        md="4"
+                    >
+                        <v-combobox
+                            label="Gender"
+                            :items="['Male', 'Female', 'LGBTQ']"
+                            variant="outlined"
+                        ></v-combobox>
+                    </v-col>
+                </v-row>
             </v-container>
         </v-main>
     </v-app>
